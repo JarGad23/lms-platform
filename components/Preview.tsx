10440e128d2d774ed65655eaaa75b1df32f5a632
JarGad23 <127743272+JarGad23@users.noreply.github.com>
2023-10-04 18:37:47 +0200

    Feature 17: Description and access forms creation (#17)
    * Package Json changes
    
    * CSS globals changes
    
    * Modified chapter id page
    
    * Chapter description and access forms creation
    
    * Added editor preview and checkbox components

---
+++ components/Preview.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import "react-quill/dist/quill.snow.css";
+
+interface PreviewProps {
+  value: string;
+}
+
+export const Preview: React.FC<PreviewProps> = ({ value }) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+  return <ReactQuill theme="bubble" value={value} readOnly />;
+};
