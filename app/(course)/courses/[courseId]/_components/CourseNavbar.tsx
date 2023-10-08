67f81d0349241777a83bb8781dbac4bb62240754
JarGad23 <127743272+JarGad23@users.noreply.github.com>
2023-10-08 16:12:34 +0200

    Feature 25: Courses chapters layout (#25)
    * Created Course and Chapter layout
    
    * Adde Course Id page
    
    * Added Course Sidebar and Navbar components
    
    * Prisma and Navbar changes

---
+++ app/(course)/courses/[courseId]/_components/CourseNavbar.tsx
@@ -0,0 +1,24 @@
+import { NavbarRoutes } from "@/components/NavbarRoutes";
+import { Chapter, Course, UserProgress } from "@prisma/client";
+import { CourseMobileSidebar } from "./CourseMobileSidebar";
+
+interface CourseNavbarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+export const CourseNavbar: React.FC<CourseNavbarProps> = ({
+  course,
+  progressCount,
+}) => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
+      <NavbarRoutes />
+    </div>
+  );
+};
