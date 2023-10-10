c391aaad04ff7e5e1cca2a5ce74c9ae91ca3236e
JarGad23 <j.gad@m8b.pl>
2023-10-10 07:50:49 +0200

    Modifications in existing components

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { IconBadge } from "./IconBadge";
 import { BookOpen } from "lucide-react";
 import { formatPrice } from "@/lib/format";
+import { CourseProgress } from "./CourseProgress";
 
 interface CourseCardProps {
   id: string;
@@ -43,7 +44,11 @@ export const CourseCard: React.FC<CourseCardProps> = ({
             </div>
           </div>
           {progress !== null ? (
-            <div>Progress Component</div>
+            <CourseProgress
+              size="sm"
+              value={progress}
+              variant={progress === 100 ? "success" : "default"}
+            />
           ) : (
             <p className="text-md md:text-sm font-medium text-slate-700">
               {formatPrice(price)}
