2394c0c4c5507ea265f230e0404b89429a6568de
JarGad23 <j.gad@m8b.pl>
2023-10-20 16:20:38 +0200

    Add missing apos

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterVideoForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterVideoForm.tsx
@@ -87,7 +87,7 @@ export const ChapterVideoForm: React.FC<ChapterVideoFormProps> = ({
             }}
           />
           <div className="text-xs text-muted-foreground mt-4">
-            Upload this chapter's video
+            Upload this chapter&apos;s video
           </div>
         </div>
       )}
